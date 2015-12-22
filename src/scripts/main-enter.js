80f8d7f8b71ee1e04c65f764c6b61b632258c192
Alex Bezuska <abezuska@gmail.com>
2015-12-21 19:47:48 -0500

    spawn grass tiles

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,4 +1,37 @@
 "use strict";
 
+var prefabs = require("../data/prefabs");
+
+function randomRange(min, max) {
+	return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
+
+function clone(obj) {
+	return JSON.parse(JSON.stringify(obj)); // gross
+}
+
+function makePrefab(name, entities) {
+	var prefab = prefabs[name];
+	var id = entities.create();
+	Object.keys(prefab).forEach(function(key) {
+		if (key === "id") {
+			return;
+		}
+		entities.set(id, key, clone(prefab[key]));
+	});
+	return id;
+}
+
+function spawnRandomly(entities, quantity, prefab, position, size) {
+	for(var i =0; i < quantity; i++){
+		var entity = makePrefab(prefab, entities);
+		var newX = randomRange(position.x, size.width + position.x);
+		var newY = randomRange(position.y, size.height + position.y);
+		entities.set(entity, "position", {"x": newX,  "y": newY });
+	}
+}
+
 module.exports = function(data) { // eslint-disable-line no-unused-vars
+	spawnRandomly(data.entities, 20, "grass", {"x":0, "y":0}, data.canvas);
+
 };
--- src/systems/renderer/render-rectangles.js
+++ src/systems/renderer/render-rectangles.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-	data.entities.registerSearch("renderRectangles", ["position", "size"]);
+	data.entities.registerSearch("renderRectangles", ["position", "size", "color"]);
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
 		var position = data.entities.get(entity, "position");
 		var size = data.entities.get(entity, "size");
