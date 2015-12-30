a9569b9e1bebfa56bb4f04ce209a1943e7d9a715
Alex Bezuska <abezuska@gmail.com>
2015-12-30 13:48:34 -0500

    switch to splat-ecs 2

--- src/game.js
+++ src/game.js
@@ -20,7 +20,39 @@ sounds.loadFromManifest(require("./data/sounds"));
 
 var systems = require("./data/systems");
 
-var game = new Splat.Game(canvas, animations, entities, images, input, require, scenes, sounds, systems);
+// This is some webpack magic to ensure the dynamically required scripts are loaded
+
+var splatSystemPath = "splat-ecs/lib/systems";
+// WARNING: can't use splatSystemPath variable here, or webpack won't pick it up
+var splatSystemRequire = require.context("splat-ecs/lib/systems", true, /\.js$/);
+
+var localSystemPath = "./systems";
+var localSystemRequire = require.context("./systems", true, /\.js$/);
+
+var localScriptPath = "./scripts";
+var localScriptRequire = require.context("./scripts", true, /\.js$/);
+
+function customRequire(path) {
+	if (path.indexOf(splatSystemPath) === 0) {
+		var splatName = "./" + path.substr(splatSystemPath.length + 1) + ".js";
+		return splatSystemRequire(splatName);
+	}
+	if (path.indexOf(localSystemPath) === 0) {
+		var localName = "./" + path.substr(localSystemPath.length + 1) + ".js";
+		return localSystemRequire(localName);
+	}
+	if (path.indexOf(localScriptPath) === 0) {
+		var scriptName = "./" + path.substr(localScriptPath.length + 1) + ".js";
+		return localScriptRequire(scriptName);
+	}
+	console.error("Unable to load module: \"", path, "\"");
+	return undefined;
+}
+require("./index.html");
+require.context("./images", true, /\.(jpe?g|png|gif|svg)$/i);
+require.context("./sounds", true, /\.(mp3|ogg|wav)$/i);
+
+var game = new Splat.Game(canvas, animations, entities, images, input, customRequire, scenes, sounds, systems);
 
 function percentLoaded() {
 	if (images.totalImages + sounds.totalSounds === 0) {
--- src/systems/simulation/player-animations.js
+++ src/systems/simulation/player-animations.js
@@ -4,6 +4,17 @@ module.exports = function(ecs, data) {
 	ecs.addEach(function(player, elapsed) { // eslint-disable-line no-unused-vars
 		var playerFacing = data.entities.get(player, "facing");
 		var playerAnimation = data.entities.get(player, "animation");
+
+		if( data.input.button("up")
+		|| data.input.button("right")
+		|| data.input.button("down")
+		|| data.input.button("left")
+		|| data.input.button("action") ){
+			playerAnimation.speed = 1;
+		}else{
+			playerAnimation.speed = 0;
+		}
+
 		if(playerFacing === "down"){
 			if(data.input.button("action")){
 				playerAnimation.name = "player-slap-down";
