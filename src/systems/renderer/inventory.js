71d9bfa0b2753e622deb6beb297f0978ead5db95
Alex Bezuska <abezuska@gmail.com>
2016-01-01 23:01:15 -0500

    add inventory slot art

--- src/systems/renderer/inventory.js
+++ src/systems/renderer/inventory.js
@@ -3,12 +3,23 @@
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function collect(player, context) { // eslint-disable-line no-unused-vars
 		var inventory = data.entities.get(player, "inventory");
+		var slot = data.images.get("slot");
 		var podIcon = data.images.get("pod");
 
+		var inventoryWidth = inventory.total * slot.width;
+		var inventoryX = (data.canvas.width - inventoryWidth) / 2;
+
+		for (var i = 0; i < inventory.total; i++) {
+			var position = {
+				"x": inventoryX + (slot.width * i),
+				"y": (data.canvas.height - slot.height) - 20
+			};
+			context.drawImage(slot, position.x, position.y);
+		}
 		for (var i = 0; i < inventory.used; i++) {
 			var position = {
-				"x": 300 + (podIcon.width * i),
-				"y": (data.canvas.height - podIcon.height) - 20
+				"x": ((slot.width/2) - (podIcon.width/2) + inventoryX ) + (slot.width * i),
+				"y": (data.canvas.height - slot.height) - (20 - podIcon.height/2)
 			};
 			context.drawImage(podIcon, position.x, position.y);
 		}
